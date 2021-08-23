0d2e219816756d32e136028da77bf1bffa9fbea1
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-08-23 16:45:36 +0200

    break: make it possible to DL subs from season-less shows

--- src/crunchyrollsubdownloader/crdl.py
+++ src/crunchyrollsubdownloader/crdl.py
@@ -42,6 +42,15 @@ def get_season_episodes_url(soup, show_url, season_name):
     return episodes_url
 
 
+def get_all_episodes_url(soup, show_url):
+    seasons = soup_get_list_of_seasons(soup)
+    episodes = seasons.find_all("a", {"class": "episode"})
+    crunchyroll_url = calc_root_url(show_url)
+    episodes_url = ["{}{}".format(crunchyroll_url, ep["href"])
+                    for ep in episodes]
+    return episodes_url
+
+
 def get_list_of_seasons(soup):
     seasons = soup_get_list_of_seasons(soup)
     seasons_list = seasons.find_all("a", {"class": "season-dropdown"})
@@ -52,26 +61,29 @@ def main():
 
     parser = argparse.ArgumentParser(
         add_help="Downloads a full season subtitles pack from Crunchyroll, with youtube-dl")
-    parser.add_argument("--flaresolverr", help="Flaresolverr API URL. Default: http://localhost:8191",
+    parser.add_argument("-f", "--flaresolverr", help="Flaresolverr API URL. Default: http://localhost:8191",
                         type=str, default="http://localhost:8191")
-    parser.add_argument('-v', help="Verbose mode",
+    parser.add_argument('-v', "--verbose", help="Verbose mode",
                         action='store_const', default=False, const=True)
 
     subparsers = parser.add_subparsers(help="Subcommands", dest="command")
 
+    # DL
     parser_dl = subparsers.add_parser(
         'dl', help="Directly download subtitles with a given season name and language")
     parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
     parser_dl.add_argument(
-        'season_name', help="Crunchyroll season name on the webpage", type=str)
+        "-s", '--season_name', help="Crunchyroll season name on the webpage", type=str)
     parser_dl.add_argument('lang', help="Crunchyroll language", type=str)
 
+    # LANG
     parser_dl = subparsers.add_parser(
         'lang', help="Shows list of available subtitles for a given show and season name")
     parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
     parser_dl.add_argument(
-        'season_name', help="Crunchyroll season name on the webpage", type=str)
+        "-s", '--season_name', help="Crunchyroll season name on the webpage", type=str)
 
+    # SEASONS
     parser_dl = subparsers.add_parser(
         'seasons', help="Shows list of seasons for a given show")
     parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
@@ -86,10 +98,14 @@ def main():
     soup = fetch_url(args.flaresolverr, args.show_url)
 
     if(args.command == "dl"):
-        episodes = get_season_episodes_url(
-            soup, args.show_url, args.season_name)
+        if(args.season_name is None):
+            episodes = get_all_episodes_url(soup, args.show_url)
+        else:
+            episodes = get_season_episodes_url(
+                soup, args.show_url, args.season_name)
+
         ytdl = youtube_dl.YoutubeDL(
-            {"writesubtitles": True, "subtitleslangs": [args.lang], "skip_download": True, "quiet": not(args.v), "no_warnings": not(args.v)})
+            {"writesubtitles": True, "subtitleslangs": [args.lang], "skip_download": True, "quiet": not(args.verbose), "no_warnings": not(args.verbose)})
         for el in episodes:
             try:
                 ytdl.download([el])
@@ -104,10 +120,14 @@ def main():
             print(el)
 
     elif(args.command == "lang"):
-        episodes = get_season_episodes_url(
-            soup, args.show_url, args.season_name)
+        if(args.season_name is None):
+            episodes = get_all_episodes_url(soup, args.show_url)
+        else:
+            episodes = get_season_episodes_url(
+                soup, args.show_url, args.season_name)
+
         ytdl = youtube_dl.YoutubeDL(
-            {"writesubtitles": True, "quiet": not(args.v), "no_warnings": not(args.v)})
+            {"writesubtitles": True, "quiet": not(args.verbose), "no_warnings": not(args.verbose)})
         if(len(episodes) == 0):
             print("No episode found for this show/season")
             exit()

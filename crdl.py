8647c09863e6a94340b811f092217e5da17f686f
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2021-08-23 12:44:43 +0200

    Added main method

--- crdl.py
+++ crdl.py
@@ -50,64 +50,72 @@ def get_list_of_seasons(soup):
     return [el["title"] for el in seasons_list]
 
 
-parser = argparse.ArgumentParser(
-    add_help="Downloads a full season subtitles pack from Crunchyroll, with youtube-dl")
-parser.add_argument("--flaresolverr", help="Flaresolverr API URL. Default: http://localhost:8191",
-                    type=str, default="http://localhost:8191")
-parser.add_argument('-v', help="Verbose mode",
-                    action='store_const', default=False, const=True)
+def main():
 
-subparsers = parser.add_subparsers(help="Subcommands", dest="command")
+    parser = argparse.ArgumentParser(
+        add_help="Downloads a full season subtitles pack from Crunchyroll, with youtube-dl")
+    parser.add_argument("--flaresolverr", help="Flaresolverr API URL. Default: http://localhost:8191",
+                        type=str, default="http://localhost:8191")
+    parser.add_argument('-v', help="Verbose mode",
+                        action='store_const', default=False, const=True)
 
-parser_dl = subparsers.add_parser(
-    'dl', help="Directly download subtitles with a given season name and language")
-parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
-parser_dl.add_argument(
-    'season_name', help="Crunchyroll season name on the webpage", type=str)
-parser_dl.add_argument('lang', help="Crunchyroll language", type=str)
+    subparsers = parser.add_subparsers(help="Subcommands", dest="command")
 
-parser_dl = subparsers.add_parser(
-    'lang', help="Shows list of available subtitles for a given show and season name")
-parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
-parser_dl.add_argument(
-    'season_name', help="Crunchyroll season name on the webpage", type=str)
+    parser_dl = subparsers.add_parser(
+        'dl', help="Directly download subtitles with a given season name and language")
+    parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
+    parser_dl.add_argument(
+        'season_name', help="Crunchyroll season name on the webpage", type=str)
+    parser_dl.add_argument('lang', help="Crunchyroll language", type=str)
 
-parser_dl = subparsers.add_parser(
-    'seasons', help="Shows list of seasons for a given show")
-parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
+    parser_dl = subparsers.add_parser(
+        'lang', help="Shows list of available subtitles for a given show and season name")
+    parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
+    parser_dl.add_argument(
+        'season_name', help="Crunchyroll season name on the webpage", type=str)
 
-args = parser.parse_args()
+    parser_dl = subparsers.add_parser(
+        'seasons', help="Shows list of seasons for a given show")
+    parser_dl.add_argument('show_url', help="Crunchyroll show URL", type=str)
 
-soup = fetch_url(args.flaresolverr, args.show_url)
+    args = parser.parse_args()
 
-if(args.command == "dl"):
-    episodes = get_season_episodes_url(soup, args.show_url, args.season_name)
-    ytdl = youtube_dl.YoutubeDL(
-        {"writesubtitles": True, "subtitleslangs": [args.lang], "skip_download": True, "quiet": not(args.v), "no_warnings": not(args.v)})
-    for el in episodes:
-        try:
-            ytdl.download([el])
-        except:
-            print("Can't download episode subtitles from {}, skipping".format(el))
+    soup = fetch_url(args.flaresolverr, args.show_url)
 
-elif(args.command == "seasons"):
-    seasons = get_list_of_seasons(soup)
-    print("List of available seasons to download:")
-    print("---------------------------------------")
-    for el in seasons:
-        print(el)
+    if(args.command == "dl"):
+        episodes = get_season_episodes_url(
+            soup, args.show_url, args.season_name)
+        ytdl = youtube_dl.YoutubeDL(
+            {"writesubtitles": True, "subtitleslangs": [args.lang], "skip_download": True, "quiet": not(args.v), "no_warnings": not(args.v)})
+        for el in episodes:
+            try:
+                ytdl.download([el])
+            except:
+                print("Can't download episode subtitles from {}, skipping".format(el))
 
-elif(args.command == "lang"):
-    episodes = get_season_episodes_url(soup, args.show_url, args.season_name)
-    ytdl = youtube_dl.YoutubeDL(
-        {"writesubtitles": True, "quiet": not(args.v), "no_warnings": not(args.v)})
-    if(len(episodes) == 0):
-        print("No episode found for this show/season")
-        exit()
-    # Checking last item in the list, cause the first item is the most recent one and can be paywalled
-    meta = ytdl.extract_info(episodes[-1], False)
-    languages = meta["subtitles"]
-    print("List of available languages to download:")
-    print("---------------------------------------")
-    for el in languages.keys():
-        print(el)
+    elif(args.command == "seasons"):
+        seasons = get_list_of_seasons(soup)
+        print("List of available seasons to download:")
+        print("---------------------------------------")
+        for el in seasons:
+            print(el)
+
+    elif(args.command == "lang"):
+        episodes = get_season_episodes_url(
+            soup, args.show_url, args.season_name)
+        ytdl = youtube_dl.YoutubeDL(
+            {"writesubtitles": True, "quiet": not(args.v), "no_warnings": not(args.v)})
+        if(len(episodes) == 0):
+            print("No episode found for this show/season")
+            exit()
+        # Checking last item in the list, cause the first item is the most recent one and can be paywalled
+        meta = ytdl.extract_info(episodes[-1], False)
+        languages = meta["subtitles"]
+        print("List of available languages to download:")
+        print("---------------------------------------")
+        for el in languages.keys():
+            print(el)
+
+
+if __name__ == "__main__":
+    main()
